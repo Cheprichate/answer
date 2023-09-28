ec802da4f6d4b744ab36735fa0697dbcaf695927
Cheprichate <petrovich15071998@mail.ru>
2023-09-28 21:03:44 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+int a ;
+int b ;
+int ans ;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите первое число : ";
+    std::cin >> a ;
+    std::cout << "Введите второе число : ";
+    std::cin >> b ;
+    std::cout << "Введите их сумму : ";
+    std::cin >> ans;
+
+    if (a + b == ans){
+        std::cout << "Ответ верный";
+    }
+    else {
+        std::cout << "Ответ не верный";
+    }
+}
